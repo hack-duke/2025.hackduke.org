4565d59f930e75ff77e48c4eeb7b0180d43e42c5
Arvindh Manian <arvindh.manian@gmail.com>
2024-10-08 12:47:56 -0400

    feat: fill out skeleton

---
+++ src/components/navbar.js
@@ -0,0 +1,1 @@
+// code for the navbar should go here
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      primary: '#5A26A1',
+      secondary: '#9DD6FB',
+      ter: '#DE86E9',
+      background: '#193D91',
+      white: '#FFF',
+      accent2: '#FEF751',
+      accent3: '#DE035E',
+      black: '#170738'
+    },
+    extend: {}
+  },
+  plugins: []
+}

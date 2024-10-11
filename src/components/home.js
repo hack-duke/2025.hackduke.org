86992cf0f6989962fe0675267213604569e3fce6
maxx06 <67605840+maxx06@users.noreply.github.com>
2024-10-11 13:31:31 -0400

    img

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,12 @@
 export const Home = () => {
   return (
-    <section className='bg-accent1 w-screen text-white'>Home Section</section>
+    <div>
+      <section className='bg-background w-screen text-white'>
+        <div className='flex flex-col items-center justify-center h-screen'>
+          <h1 className='text-5xl'>Welcome to Hackathon Starter</h1>
+          <p className='text-xl'>This is a starter template for a hackathon website</p>
+        </div>
+      </section>
+    </div>
   )
 }

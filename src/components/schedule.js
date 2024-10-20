c208afc1ac27d7938d7be6db658cef2229bc822d
maxx06 <67605840+maxx06@users.noreply.github.com>
2024-10-20 00:51:51 -0400

    small bug fixes

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -1,6 +1,6 @@
 export const Schedule = () => {
   return (
-    <div className='flex flex-col bg-gradient-to-b from-schedule_bg1 to-schedule_bg2 w-screen text-white'>
+    <div className='flex flex-col bg-gradient-to-b from-schedule_bg1 to-schedule_bg2 w-full text-white'>
       <p>schedule</p>
       <img src="/images/Shark.png"/>
     </div>
--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -1,6 +1,6 @@
 export const Sponsors = () => {
   return (
-    <div className='bg-gradient-to-b from-sponsors_bg1 to-sponsors_bg2 min-h-full w-screen text-white h-64'>
+    <div className='bg-gradient-to-b from-sponsors_bg1 to-sponsors_bg2 min-h-full w-full text-white h-64'>
       <p>sponsors</p>
     </div>
   )

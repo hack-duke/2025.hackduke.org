4d66066debcae21fac9e7c88e346f52695ee0321
Arvindh Manian <arvindh.manian@gmail.com>
2024-10-20 15:58:14 -0400

    fix class to classname

--- src/components/tracks.js
+++ src/components/tracks.js
@@ -1,9 +1,17 @@
 export const Tracks = () => {
   return (
     <div className='h-screen text-white h-64'>
-      <img src="/images/RightWallFront.png" alt="Description" class="absolute -right-20 transform -translate-x-1/2 w-40 max-h-45" />
-      <img src="/images/LeftWallFront.png" alt="Description" class="absolute left-0 transform translate-x-0 w-40 max-h-25" />
-    
+      <img
+        src='/images/RightWallFront.png'
+        alt='Description'
+        className='absolute -right-20 transform -translate-x-1/2 w-40 max-h-45'
+      />
+      <img
+        src='/images/LeftWallFront.png'
+        alt='Description'
+        className='absolute left-0 transform translate-x-0 w-40 max-h-25'
+      />
+
       <p>tracks</p>
     </div>
   )

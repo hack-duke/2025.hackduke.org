00305db291db9b2d1f6bc2155bf33e67513fce54
maxx06 <67605840+maxx06@users.noreply.github.com>
2024-10-22 21:38:24 -0400

    buggy buoy

--- src/components/home.js
+++ src/components/home.js
@@ -10,10 +10,9 @@ export const Home = () => {
         {/* <div className="wave wave3"></div>
         <div className="wave wave4"></div> */}
       </section>
-      {/* <img src="/images/waves/wave1.png" alt="Description" className="wave1  absolute -bottom-12 left-1/2 transform -translate-x-1/2 w-full max-h-45 flex-grow" /> */}
 
+      <img src="/images/Buoy.png" alt="Buoy" className="buoy" />
 
-      <img src="/images/Buoy.png" alt="Description" className="order-first absolute bottom-0 left-1/2 transform -translate-x-1/2 w-full max-h-45" />
       <img src="/images/HomeStars.png" alt="Description" className="order-first absolute top-12 left-1/2 transform -translate-x-1/2 w-full max-h-45" />
       <img src="/images/Blur1.png" alt="Description" className="z-100 absolute top-12 left-1/2 transform -translate-x-1/2 w-full max-h-45" />
       <img 

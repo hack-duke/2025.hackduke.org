43ce4dc74113ff500f83adad4c89f0d63e745dff
maxx06 <67605840+maxx06@users.noreply.github.com>
2024-10-29 19:26:03 -0400

    lighting + adjustments

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,10 @@
 export const Footer = () => {
     return (
         <div className='relative w-full text-white'>
-            <p className='absolute z-100'>Made with 🩷 by the HackDuke team</p>
+            <p className='absolute z-100 bottom-[5rem]'>Made with 🩷 by the HackDuke team</p>
             <img 
             src="/images/AnglerFish.png" 
-            className='absolute z-50 h-auto w-[43rem] bottom-[12rem] right-[5rem]' 
+            className='absolute z-50 h-auto w-[43rem] bottom-[15rem] right-[5rem]' 
             alt="Angler Fish"/>
 
             <img 
@@ -18,19 +18,39 @@ export const Footer = () => {
             alt="Cliff Covering Spaceship"/>
 
             <img 
+            src="/images/SpaceshipLighting.png" 
+            className='absolute z-20 h-auto w-[78rem] bottom-[-1rem] right-[1rem]' 
+            alt="Lighting"/>
+
+            <img 
+            src="/images/MidLighting.png" 
+            className='absolute z-20 h-auto w-[78rem] bottom-[-1rem] right-[-1rem]' 
+            alt="Lighting"/>
+
+            <img 
             src="/images/Blue Coral.png" 
-            className='absolute z-40 h-auto w-[20rem] bottom-[-1rem] right-[-1rem]' 
+            className='absolute z-40 h-auto w-[20rem] bottom-[20rem] left-[-1rem] opacity-50' 
             alt="Blue Coral"/>
 
             <img 
+            src="/images/Purple Coral.png" 
+            className='absolute z-40 h-auto w-[15rem] bottom-[20rem] left-[3rem] opacity-50' 
+            alt="Purple Coral"/>
+
+            <img 
             src="/images/Coral Cliff.png" 
-            className='absolute z-30 h-auto w-[35rem] bottom-[-1rem] left-[-1.23rem]' 
+            className='absolute z-10 h-auto w-[35rem] bottom-[-1rem] left-[-1.23rem]' 
             alt="Coral Cliff"/>
 
             <img 
             src="/images/Crab Cliff.png" 
-            className='absolute z-30 h-auto w-[35rem] bottom-[-1rem] left-[-1.23rem]' 
+            className='absolute z-10 h-auto w-[50rem] bottom-[-1rem] left-[-1.23rem]' 
             alt="Crab Cliff"/>
+
+            <img 
+            src="/images/Crab.png" 
+            className='absolute z-30 h-auto w-[15rem] bottom-[10rem] left-[3.23rem] opacity-30' 
+            alt="Crab Cliff"/>  
             
         </div>
     )

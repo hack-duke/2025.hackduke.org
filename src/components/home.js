4429dc1e933a017466feba6b0ac1d8ac1d49c09a
rally.lin@duke.edu <linrally@gmail.com>
2024-11-10 16:20:30 -0500

    buoy fixes

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,7 @@ export const Home = () => {
     <div className="flex flex-col items-center justify-center h-screen  bg-gradient-to-b from-home_bg1 to-home_bg2 text-white overflow-hidden">
       <img className="z-20 flex object-scale-down max-h-full h-48" src="/images/HackDukeLogo.png" alt="Hack Duke"></img>
       {/* waves */}
-      <section className="waves-container -bottom-12">
+      <section className="waves-container bottom-[-2vw]">
         <div className="wave wave1"></div>
         <div className="wave wave2"></div>
         {/* <div className="wave wave3"></div>

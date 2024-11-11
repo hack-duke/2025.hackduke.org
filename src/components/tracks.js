89ef848a0a8f2f5f989c3e3f0ac0a2bb66ddec4a
Arvindh Manian <arvindh.manian@gmail.com>
2024-11-11 16:51:47 -0500

    Add responsiveness

--- src/components/tracks.js
+++ src/components/tracks.js
@@ -4,8 +4,8 @@ import { H1 } from "./typography"
 const Track = ({ title, image_src, image_alt, widthClass, leftClass, bottomClass, rightClass, topClass, imageWidthClass }) => {
   // Pass class names instead of, e.g., vw units because Tailwind has issues with template literals
   return (
-    <div className={"absolute z-20 flex flex-col space-y-2 items-center" + [widthClass, leftClass, bottomClass, rightClass,topClass].join(" ")}>
-      <h3 className="text-3xl text-white text-center">{title}</h3>
+    <div className={"absolute z-20 flex flex-col space-y-2 items-center " + [widthClass, leftClass, bottomClass, rightClass,topClass].join(" ")}>
+      <h3 className="text-sm md:text-3xl text-white text-center">{title}</h3>
       <img src={image_src} alt={image_alt} className={imageWidthClass}/>
     </div>
   )
@@ -17,15 +17,15 @@ export const Tracks = () => {
   // TODO: Lighting
   // TODO: Responsiveness
   return (
-    <Panel className="h-[80vh]">
+    <Panel className="h-[40vh] md:h-[80vh]">
       <H1 className="text-center">Tracks</H1>
       <Track 
         title="Interactive Media"
         image_src="/images/TracksCrab.svg"
         image_alt="Crab"
-        widthClass="w-[20vw]"
+        widthClass="w-[35vw] md:w-[20vw]"
         imageWidthClass="w-[13vw]"
-        rightClass="right-[7.5vw]"
+        rightClass="right-[15vw] md:right-[7.5vw]"
         bottomClass="bottom-[5vh]"
         leftClass="left-auto"
         topClass="top-auto"
@@ -38,7 +38,7 @@ export const Tracks = () => {
         image_alt="Generic Fish" 
         widthClass="w-[18vw]" 
         imageWidthClass="w-[18vw]"
-        leftClass="left-[25vw]" 
+        leftClass="left-[5vw] md:left-[25vw]" 
         bottomClass="bottom-[3vh]" 
         rightClass="right-auto" 
         topClass="top-auto" 
@@ -51,7 +51,7 @@ export const Tracks = () => {
         widthClass="w-[12vw]"
         imageWidthClass="w-[12vw]"
         leftClass="left-[14vw]"
-        topClass="top-[20vh]"
+        topClass="top-[10vh] md:top-[20vh]"
         rightClass="right-auto"
         bottomClass="bottom-auto"
       />
@@ -62,8 +62,8 @@ export const Tracks = () => {
         image_alt="Whale"
         widthClass="w-[25vw]"
         imageWidthClass="w-[25vw]"
-        rightClass="right-[27vw]"
-        topClass="top-[20vh]"
+        rightClass="right-[10vw] md:right-[27vw]"
+        topClass="top-[10vh] md:top-[20vh]"
         leftClass="left-auto"
         bottomClass="bottom-auto"
       />

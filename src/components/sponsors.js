342e121d2cd2bebfb8bd11b073817f92e159f0eb
maxx06 <67605840+maxx06@users.noreply.github.com>
2024-11-17 13:54:38 -0500

    Update sponsors.js

--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -15,49 +15,49 @@ export const Sponsors = () => {
             src='/images/Optiver.svg'
             alt='Optiver'
             className='w-full h-auto z-50'
-            style={{ gridColumn: '1', gridRow: '1' }}
+            style={{ gridColumn: '1', gridRow: '1 / span 2'}}
           />
           {/* HRT */}
           <img
             src='/images/HRT.svg'
             alt='HRT'
             className='w-full h-auto z-50'
-            style={{ gridColumn: '2', gridRow: '1' }}
+            style={{ gridColumn: '2'}}
           />
           {/* IMC */}
           <img
             src='/images/IMC.svg'
             alt='IMC'
             className='w-full h-auto z-50'
-            style={{ gridColumn: '3', gridRow: '1' }}
+            style={{ gridColumn: '3'}}
           />
           {/* DRW */}
           <img
             src='/images/DRW.svg'
             alt='DRW'
-            className='w-full h-auto z-50'
-            style={{ gridColumn: '1', gridRow: '2' }}
+            className='w-full h-auto z-50 translate-y-[-76%]'
+            style={{ gridColumn: '1'}}
           />
           {/* Roblox */}
           <img
             src='/images/Roblox.svg'
             alt='Roblox'
-            className='w-full h-auto z-50'
-            style={{ gridColumn: '2', gridRow: '2' }}
+            className='w-full h-auto z-50 translate-y-[-36%]'
+            style={{ gridColumn: '2'}}
           />
           {/* Duke I&E */}
           <img
             src='/images/Duke%20I&E.svg'
             alt='Duke I&E'
-            className='w-full h-auto z-50'
-            style={{ gridColumn: '3', gridRow: '2' }}
+            className='w-full h-auto z-50 translate-y-[-110%]'
+            style={{ gridColumn: '3'}}
           />
           {/* Pinecone */}
           <img
             src='/images/Pinecone.svg'
             alt='Pinecone'
-            className='w-full h-auto z-50'
-            style={{ gridColumn: '2', gridRow: '3' }}
+            className='w-full h-auto z-50 translate-y-[-113%]'
+            style={{ gridColumn: '2'}}
           />
         </div>
       </div>

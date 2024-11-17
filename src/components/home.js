dcb996c22351f278fa931f09766d9ab285180fec
Rishi Rao <22raor@gmail.com>
2024-11-17 15:56:46 -0500

    trust

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,6 @@
 export const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen  bg-gradient-to-b from-home_bg1 to-home_bg2 text-white overflow-hidden w-full position-relative">
+    <div className="flex flex-col items-center justify-center h-screen  bg-gradient-to-b from-home_bg1 to-home_bg2 text-white overflow-hidden w-full position-relative max-w-full">
       <img className="z-20 flex object-contain max-h-full h-48" src="/images/HackDuke.png" alt="Hack Duke"></img>
       {/* waves */}
       <section className="waves-container bottom-[0vw]">
@@ -9,7 +9,9 @@ export const Home = () => {
         {/* <div className="wave wave3"></div>
         <div className="wave wave4"></div> */}
       </section>
-      <img src="/images/Buoy.svg" alt="Description" className="buoy z-10" />
+      <div className="buoy-wrapper overflow-hidden absolute w-full h-full">
+        <img src="/images/Buoy.svg" alt="Description" className="buoy z-10" />
+      </div>
       <img src="/images/HomeStars.png" alt="Description" className="order-first absolute top-12 left-1/2 transform -translate-x-1/2 w-full max-h-45" />
       {/* <img 
         src="/images/CodeForGood.png" 

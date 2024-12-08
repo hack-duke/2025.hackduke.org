2dc5b6a87d8a4ae02b04c7e02804c7de7ee2a76b
Janet Meng <jm914@duke.edu>
2024-12-08 17:40:59 -0500

    Update schedule info, reduce font size

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -1,14 +1,24 @@
-import { Panel } from "./panel"
-import { H1 } from "./typography"
+import { Panel } from "./panel";
+import { H1 } from "./typography";
 
 const ScheduleItem = ({ time, event }) => {
+  const formattedEvent =
+    event.length > 28
+      ? `${event.slice(0, 28)}\n${event.slice(28)}`
+      : event;
+
   return (
-    <div className='flex items-center'>
-      <span className='text-[1.5vw] text-[#FBF9F7] font-bold w-[13vw]'>{time}</span>
-      <span className='text-[1.5vw] text-[#FBF9F7]'>{event}</span>
+    <div className="flex items-start">
+      <span className="text-[1.05vw] text-[#FBF9F7] font-bold w-[13vw]">{time}</span>
+      <span 
+        className="text-[1.05vw] text-[#FBF9F7] whitespace-pre-line"
+      >
+        {formattedEvent}
+      </span>
     </div>
-  )
-}
+  );
+};
+
 
 const DaySchedule = ({ date, events, isSecondDay }) => {
   return (
@@ -27,23 +37,27 @@ const DaySchedule = ({ date, events, isSecondDay }) => {
 
 export const Schedule = () => {
   const day1Events = [
-    { time: '9:30-10:30 am', event: 'Student Check-in' },
-    { time: '11:00-12:15 am', event: 'Opening Ceremony' },
-    { time: '12:15-1:00 pm', event: 'Lunch' },
-    { time: '12:15-1:00 pm', event: 'Team Formation Mixer' },
-    { time: '2:00-3:00 pm', event: 'Track Talks' },
-    { time: '4:00-5:00 pm', event: 'UI/UX Design Workshop' },
-    { time: '5:00-6:00 pm', event: 'Dinner' }
+    { time: '09:00-11:00 am', event: 'Student Check-in' },
+    { time: '10:30-11:30 am', event: 'Opening Ceremony' },
+    { time: '11:30 am', event: 'Hacking Starts'},
+    { time: '11:30-12:30 pm', event: 'Team Formation & Lunch' },
+    { time: '01:00-03:00 pm', event: 'Sponsor Fair @ Kirby Winter Garden'},
+    { time: '03:00-04:00 pm', event: 'Workshop 1 @ Room _' },
+    { time: '04:00-05:00 pm', event: 'Workshop 2 @ Room _' },
+    { time: '05:00-06:00 pm', event: 'Workshop 3 @ Room _' },
+    { time: '06:00-07:00 pm', event: 'Dinner' },
+    { time: '07:00-08:00 pm', event: 'Social Event 1 @ _' },
+    { time: '08:00-09:00 pm', event: 'Social Event 2 @ _' },
+    { time: '09:00-10:00 pm', event: 'Social Event 3 @ _' },
   ]
 
   const day2Events = [
-    { time: '2:00-1:00 am', event: 'Midnight Snack' },
-    { time: '9:00-10:00 am', event: 'Breakfast' },
-    { time: '11:00-12:00 pm', event: 'Lunch' },
-    { time: '11:00-12:30 pm', event: 'Hacking Ends' },
-    { time: '1:00-2:00 pm', event: 'Pitching Workshop' },
+    { time: '12:00-1:30 am', event: 'Midnight Breakfast ' },
+    { time: '10:00-1:00 pm', event: 'Brunch' },
+    { time: '11:30 pm', event: 'Hacking Ends' },
+    { time: '12:00-1:00 pm', event: 'Pitching Workshop @ Room _' },
     { time: '2:00-4:30 pm', event: 'Judging' },
-    { time: '6:00-6:30 pm', event: 'Closing Ceremony' }
+    { time: '5 pm', event: 'Closing Ceremony @ Geneen Auditorium' }
   ]
 
   return (
@@ -54,8 +68,8 @@ export const Schedule = () => {
 
       <div className='w-screen flex justify-center'>
         <div className='flex flex-col md:flex-row gap-[3vw] max-w-[90vw] md:max-w-none'>
-          <DaySchedule date="10/22/24" events={day1Events} isSecondDay={false} />
-          <DaySchedule date="10/23/24" events={day2Events} isSecondDay={true} />
+          <DaySchedule date="2/8/25" events={day1Events} isSecondDay={false} />
+          <DaySchedule date="2/9/25" events={day2Events} isSecondDay={true} />
         </div>
       </div>
     </Panel>

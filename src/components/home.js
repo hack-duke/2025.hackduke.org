88df8dc85b1cc0a5ec0e55f437940512dc1b642e
maxx06 <67605840+maxx06@users.noreply.github.com>
2024-12-25 15:07:57 +0800

    countdown timer

--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,13 @@
+import Countdown from 'react-countdown';
+
 export const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen  bg-gradient-to-b from-home_bg1 to-home_bg2 text-white overflow-hidden w-full relative max-w-full">
       <img className="z-20 flex object-contain max-h-full h-48" src="/images/HackDuke.png" alt="Hack Duke"></img>
       <h1> Application Closes in</h1>
+      <Countdown date={new Date("2025-01-20")}/>
+
+      
       {/* waves */}
       <section className="waves-container bottom-[0vw]">
         <div className="wave wave1"></div>

1f06488925eb41eea5f2a5d155250e5d91f9ae55
maxx06 <67605840+maxx06@users.noreply.github.com>
2024-12-25 14:57:05 +0800

    apply button now links to portal.hackduke.org

--- src/components/home.js
+++ src/components/home.js
@@ -2,6 +2,7 @@ export const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen  bg-gradient-to-b from-home_bg1 to-home_bg2 text-white overflow-hidden w-full relative max-w-full">
       <img className="z-20 flex object-contain max-h-full h-48" src="/images/HackDuke.png" alt="Hack Duke"></img>
+      <h1> Application Closes in</h1>
       {/* waves */}
       <section className="waves-container bottom-[0vw]">
         <div className="wave wave1"></div>
@@ -12,7 +13,7 @@ export const Home = () => {
       <div className="buoy-wrapper overflow-hidden absolute w-full h-full">
         <img src="/images/Buoy.svg" alt="Description" className="buoy z-10" />
       </div>
-      <img src="/images/HomeStars.png" alt="Description" className="motion-preset-slide-down  motion-duration-2000 order-first absolute top-12 left-1/2 transform -translate-x-1/2 w-full max-h-45" />
+      <img src="/images/HomeStars.png" alt="Description" className="motion-preset-slide-down motion-duration-2000 order-first absolute top-12 left-1/2 transform -translate-x-1/2 w-full max-h-45" />
       {/* <img 
         src="/images/CodeForGood.png" 
         alt="Code For Good" 

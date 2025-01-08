99cc72eb95383e965a5817dd4593aa129569e181
Rishi Rao <22raor@gmail.com>
2025-01-08 16:35:30 -0500

    add mlh badge

--- src/App.js
+++ src/App.js
@@ -12,7 +12,33 @@ import { Backdrop } from './components/backdrop'
 function App() {
   return (
     <>
+
+<a
+        id="mlh-trust-badge"
+        style={{
+          display: 'block',
+          maxWidth: '100px',
+          minWidth: '60px',
+          position: 'fixed',
+          left: '10px',
+          top: '0',
+          width: '10%',
+          zIndex: 10000,
+        }}
+        href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2025-season&utm_content=blue"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="https://s3.amazonaws.com/logged-assets/trust-badge/2025/mlh-trust-badge-2025-blue.svg"
+          alt="Major League Hacking 2025 Hackathon Season"
+          style={{ width: '100%' }}
+        />
+      </a>
+      
       <Navbar />
+
+      
 
       <Home />
 

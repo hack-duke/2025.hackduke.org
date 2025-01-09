1c6b44e47d022c4a677402245fc9d7a0a01dc32e
rally.lin@duke.edu <linrally@gmail.com>
2025-01-08 22:09:31 -0500

    added sponsor links

--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -3,13 +3,15 @@ import { Panel } from './panel';
 import { H1 } from './typography';
 
 // Reusable SponsorLogo Component
-const SponsorLogo = ({ src, alt, className }) => {
+const SponsorLogo = ({ src, alt, className, url }) => {
   return (
-    <img
-      src={src}
-      alt={alt}
-      className={`z-50 bg-white bg-opacity-35 p-2 rounded-md ${className}`}
-    />
+    <a href={url} className='z-50' target="_blank" rel="noopener noreferrer">
+      <img
+        src={src}
+        alt={alt}
+        className={`bg-white bg-opacity-35 p-2 rounded-md ${className} w-full`}
+      />
+    </a>
   );
 };
 
@@ -21,25 +23,26 @@ export const Sponsors = () => {
         <div class="delay-[300ms] duration-[600ms] taos:translate-y-[100%] taos:opacity-0" data-taos-offset="300">
           <div className="flex flex-row gap-[1vw] w-full">
             <div className='flex flex-1 flex-col gap-[1vw]'> {/* Don't love doing it with flex like this */}
-              <SponsorLogo src="/images/sponsors/DE_Shaw.svg" alt="DE Shaw" className='focus-visible:animate-bounce'/>
-              <SponsorLogo src="/images/sponsors/Arize.svg" alt="Arize"/>
-              <SponsorLogo src="/images/sponsors/Accenture.svg" alt="Accenture"/>
-              <SponsorLogo src="/images/sponsors/Warp.svg" alt="Warp"/>
+              <SponsorLogo src="/images/sponsors/DE_Shaw.svg" url="https://www.deshaw.com/" alt="DE Shaw" className='focus-visible:animate-bounce'/>
+              <SponsorLogo src="/images/sponsors/Arize.svg" url="https://arize.com/" alt="Arize"/>
+              <SponsorLogo src="/images/sponsors/Accenture.svg" url="https://www.accenture.com/" alt="Accenture"/>
+              <SponsorLogo src="/images/sponsors/Warp.svg" url="https://www.warp.dev/" alt="Warp"/>
             </div>
             <div className='flex flex-1 flex-col gap-[1vw]'>
-              <SponsorLogo src="/images/sponsors/Duke%20I&E.svg" alt="Duke I&E"/>
-              <SponsorLogo src="/images/sponsors/iiD.png" alt="Duke Rhodes Information Initiative"/>
-              <SponsorLogo src="/images/sponsors/CoLab.png" alt="Duke Innovation CoLab"/>
-              <SponsorLogo src="/images/sponsors/Pratt.png" alt="Pratt"/>
-              <SponsorLogo src="/images/sponsors/RedBull.svg" alt="RedBull"/>
+              <SponsorLogo src="/images/sponsors/Duke%20I&E.svg" url="https://entrepreneurship.duke.edu/" alt="Duke I&E"/>
+              <SponsorLogo src="/images/sponsors/iiD.png" url="https://bigdata.duke.edu/" alt="Duke Rhodes Information Initiative"/>
+              <SponsorLogo src="/images/sponsors/CoLab.png" url="https://colab.duke.edu/" alt="Duke Innovation CoLab"/>
+              <SponsorLogo src="/images/sponsors/Pratt.png" url="https://pratt.duke.edu/" alt="Pratt"/>
+              <SponsorLogo src="/images/sponsors/RedBull.svg" url="https://www.redbull.com/" alt="RedBull"/>
             </div>
             <div className='flex flex-1 flex-col gap-[1vw]'> {/* Don't love doing it with flex like this */}
-              <SponsorLogo src="/images/sponsors/Monster.webp" alt="Monster"/>
-              <SponsorLogo src="/images/sponsors/CardsAgainstHumanity.svg" alt="CardsAgainstHumanity"/>
+              <SponsorLogo src="/images/sponsors/Monster.webp" url="https://www.monsterenergy.com/" alt="Monster"/>
+              <SponsorLogo src="/images/sponsors/CardsAgainstHumanity.svg" url="https://www.cardsagainsthumanity.com/" alt="CardsAgainstHumanity"/>
+              {/*<SponsorLogo src="/images/sponsors/StandoutStickers.svg" url="http://hackp.ac/mlh-StandOutStickers-hackathons" alt="StandoutStickers"/>*/}
             </div>
             <div className='flex flex-[0.5] flex-col gap-[1vw]'>
-              <SponsorLogo src="/images/sponsors/Telora.jpeg" alt="Telora"/>
-              <SponsorLogo src="/images/sponsors/Keebio.webp" alt="Keebio"/>
+              <SponsorLogo src="/images/sponsors/Telora.jpeg" url="https://telora.com/" alt="Telora"/>
+              <SponsorLogo src="/images/sponsors/Keebio.webp" url="https://keeb.io/" alt="Keebio"/>
             </div>
             <div className='flex flex-[0.5]'/>
           </div>
